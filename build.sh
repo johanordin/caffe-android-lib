01296b0e0100c404504e3655db9fbb5eb1985547
jnordin20 <jnordin@fyusion.com>
2019-03-22 23:30:46 -0700

    Comment build script for testing

--- build.sh
+++ build.sh
@@ -7,21 +7,21 @@ set -eu
 
 pushd "${PROJECT_DIR}"
 
-if ! ./scripts/build_openblas.sh; then
-    echo "Failed to build OpenBLAS"
-    exit 1
-fi
+# if ! ./scripts/build_openblas.sh; then
+#     echo "Failed to build OpenBLAS"
+#     exit 1
+# fi
 
-./scripts/build_boost.sh
+# ./scripts/build_boost.sh
 ./scripts/build_gflags.sh
-./scripts/build_glog.sh
-./scripts/build_lmdb.sh
-./scripts/build_snappy.sh
-./scripts/build_leveldb.sh
-./scripts/build_opencv.sh
-./scripts/build_protobuf_host.sh
-./scripts/build_protobuf.sh
-./scripts/build_caffe.sh
+# ./scripts/build_glog.sh
+# ./scripts/build_lmdb.sh
+# ./scripts/build_snappy.sh
+# ./scripts/build_leveldb.sh
+# ./scripts/build_opencv.sh
+# ./scripts/build_protobuf_host.sh
+# ./scripts/build_protobuf.sh
+# ./scripts/build_caffe.sh
 
 popd
 
